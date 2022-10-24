37f937eed1b40d6733c7fe105d6781e9229e5ad8
Neema Velasco <neemavelasco@gmail.com>
2022-10-24 15:33:51 -0500

    fix server.js for deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,18 +3,23 @@ dotenv.config()
 
 import { Sequelize } from 'sequelize'
 
-const sequelize  = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  host: 'localhost',
-  dialect: 'mysql',
-  dialectOptions: {
-    decimalNumbers: true,
-  },
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-});
+let sequelize;
+
+process.env.JAWSDB_URL 
+  ? sequelize = new Sequelize(process.env.JAWSDB_URL)
+  : sequelize  = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+})
+
 
 export default sequelize
--- server/server.js
+++ server/server.js
@@ -7,20 +7,20 @@ dotenv.config()
 
 const app = express()
 
-// import { dirname } from 'path'
-// import { fileURLToPath } from 'url'
-// import path from 'path'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 import positionsRouter from './routes/positionsRoutes.js'
 import menuItemRouter from './routes/menuItemsRoutes.js'
 import authRouter from './routes/authRoutes.js'
 
-// const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const path = __dirname + '/views/'
 
 // only when ready to deploy
-// app.use(express.static(path.resolve(__dirname, '../client/build')))
+app.use(express.static(path))
 
-// app.use(express.static(path))
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -34,9 +34,9 @@ app.get('/', (req,res) => {
 })
 
 // only when ready to deploy
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
-// })
+app.get('*', (req, res) => {
+  res.sendFile(path + 'index.html')
+})
 
 const PORT = process.env.PORT || 8080;
 
